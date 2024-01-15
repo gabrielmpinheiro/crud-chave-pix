47d817a76f8778daee0a907a400f6cc757af599e
Gabriel Pinheiro <gabrielmpinheiro2@gmail.com>
2024-01-15 11:50:23 -0300

    Adicionando a estrutura Spring Boot Kotlin, Spring Data JPA, Spring Web, Flyway Migration

---
+++ src/main/kotlin/br/tqigmpinheiro/com/crudpix/CrudPixApplication.kt
@@ -0,0 +1,11 @@
+package br.tqigmpinheiro.com.crudpix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudPixApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudPixApplication>(*args)
+}
